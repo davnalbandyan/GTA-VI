052a298789184cdd7ed0f93f3f251b0a6830512f
davnalbandyan <davnalbandyan@gmail.com>
2024-01-04 23:20:17 +0400

    change the name of app.css to home module,move the fails to the home folder

---
+++ src/home/Home.tsx
@@ -0,0 +1,19 @@
+import './Home.module.css'
+
+
+function Home() {
+
+  return (
+    <div>
+      <form>
+        <h1>GTA VI Leave Order</h1>
+        <input type="name" placeholder='Type your name' />
+        <input type="email" placeholder='Type your email' />
+        <button>I Want GTA</button>
+      </form>
+    </div>
+  )
+
+}
+
+export default Home

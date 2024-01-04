ef556e2de354507addfab5379e48110c42b29103
davnalbandyan <davnalbandyan@gmail.com>
2024-01-05 00:00:32 +0400

    Home.tsx

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,14 +1,23 @@
+import { SubmitHandler, useForm } from 'react-hook-form'
 import './Home.module.css'
 
+interface IFormState{
+  name:string
+  email:string
+}
 
 function Home() {
+const {register,handleSubmit} = useForm<IFormState>()
 
+const onSubmit:SubmitHandler<IFormState> = (data)=> {
+  console.log(data);
+}
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <h1>GTA VI Leave Order</h1>
-        <input type="name" placeholder='Type your name' />
-        <input type="email" placeholder='Type your email' />
+        <input type="name" placeholder='Type your name' {...register("name")} />
+        <input type="email" placeholder='Type your email' {...register("email")}  />
         <button>I Want GTA</button>
       </form>
     </div>
